b41aff543bcd4429283544ac2c25a6fe42f894d4
rentaire <64122338+rentaire@users.noreply.github.com>
2020-05-30 10:59:50 +1000

    30.05.2020

---
+++ ChristmasTree/client/debug/moc_form.cpp
@@ -0,0 +1,161 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'form.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.5)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../form.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'form.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.5. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_Form_t {
+    QByteArrayData data[11];
+    char stringdata0[115];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Form_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Form_t qt_meta_stringdata_Form = {
+    {
+QT_MOC_LITERAL(0, 0, 4), // "Form"
+QT_MOC_LITERAL(1, 5, 6), // "Widget"
+QT_MOC_LITERAL(2, 12, 0), // ""
+QT_MOC_LITERAL(3, 13, 9), // "show_cart"
+QT_MOC_LITERAL(4, 23, 13), // "MyTableModel&"
+QT_MOC_LITERAL(5, 37, 7), // "myModel"
+QT_MOC_LITERAL(6, 45, 18), // "on_buy_now_clicked"
+QT_MOC_LITERAL(7, 64, 15), // "on_back_clicked"
+QT_MOC_LITERAL(8, 80, 13), // "onTextChanged"
+QT_MOC_LITERAL(9, 94, 4), // "arg1"
+QT_MOC_LITERAL(10, 99, 15) // "on_exit_clicked"
+
+    },
+    "Form\0Widget\0\0show_cart\0MyTableModel&\0"
+    "myModel\0on_buy_now_clicked\0on_back_clicked\0"
+    "onTextChanged\0arg1\0on_exit_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Form[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       6,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,   44,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       3,    1,   45,    2, 0x08 /* Private */,
+       6,    0,   48,    2, 0x08 /* Private */,
+       7,    0,   49,    2, 0x08 /* Private */,
+       8,    1,   50,    2, 0x08 /* Private */,
+      10,    0,   53,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void,
+
+ // slots: parameters
+    QMetaType::Void, 0x80000000 | 4,    5,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    9,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void Form::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Form *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->Widget(); break;
+        case 1: _t->show_cart((*reinterpret_cast< MyTableModel(*)>(_a[1]))); break;
+        case 2: _t->on_buy_now_clicked(); break;
+        case 3: _t->on_back_clicked(); break;
+        case 4: _t->onTextChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 5: _t->on_exit_clicked(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (Form::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Form::Widget)) {
+                *result = 0;
+                return;
+            }
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject Form::staticMetaObject = { {
+    &QWidget::staticMetaObject,
+    qt_meta_stringdata_Form.data,
+    qt_meta_data_Form,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *Form::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Form::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Form.stringdata0))
+        return static_cast<void*>(this);
+    return QWidget::qt_metacast(_clname);
+}
+
+int Form::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 6)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 6;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 6)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 6;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void Form::Widget()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
